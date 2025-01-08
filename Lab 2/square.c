6b4466b6142ca5aa267d525fe5ad44a691e54502
Chad Lim <chadlimjinjie@gmail.com>
2025-01-08 15:22:37 +0800

    Completed Lab 2

---
+++ Lab 2/square.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int square1(int num);
+void square2(int num, int *result);
+int main()
+{
+    int number, result = 0;
+    printf("Enter the number: \n");
+    scanf("%d", &number);
+
+    printf("square1(): %d\n", square1(number));
+    square2(number, &result);
+    printf("square2(): %d\n", result);
+    return 0;
+}
+int square1(int num)
+{
+    /* Write your code here */
+    return num * num;
+}
+void square2(int num, int *result)
+{
+    /* Write your code here */
+    *result = num * num;
+}
